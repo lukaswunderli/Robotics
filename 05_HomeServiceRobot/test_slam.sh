d107fce4b226a4d6129dbf2bb892ac218512c1f5
Lukas Wunderli <lukaswunderli@gmx.ch>
2020-06-15 16:06:49 +0000

    add test_slam.sh

---
+++ 05_HomeServiceRobot/test_slam.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#xterm -e " source devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE=$(pwd)/src/turtlebot_simulator/worlds/corridor.world; roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm -e " source devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE=$(pwd)/src/turtlebot_simulator/turtlebot_gazebo/worlds/corridor.world; roslaunch turtlebot_gazebo turtlebot_world.launch " &
+sleep 5
+xterm -e " source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch " &
+sleep 5
+xterm -e " source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+xterm -e " source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch " &
+
