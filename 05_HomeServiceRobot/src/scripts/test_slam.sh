a3747f601301d56ee4903c29be5dcedbd57fe3d5
Lukas Wunderli <lukaswunderli@gmx.ch>
2020-06-18 11:40:56 +0200

    add wall follower

--- 05_HomeServiceRobot/src/scripts/test_slam.sh
+++ 05_HomeServiceRobot/src/scripts/test_slam.sh
@@ -5,6 +5,8 @@ sleep 5
 xterm -e " source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch " &
 sleep 5
 xterm -e " source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+#sleep 5
+#xterm -e " source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch " &
 sleep 5
-xterm -e " source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch " &
+xterm -e "rosrun wall_follower wall_follower" &
 
